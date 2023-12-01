f97e5c3aafa6fbceebd510c2b0ca752488fbf0f7
Prince <prince@synthesis.co.za>
2023-12-01 12:24:30 +0200

    First Commit.

---
+++ Day 1/Day1.py
@@ -0,0 +1,13 @@
+def read_input_file:
+  file = open("Day1_Input.text","r")
+  print(file)
+
+
+def string_to_character_array:
+  return array;
+
+
+def remove_none_digit_character_:
+
+
+read_input_file()
