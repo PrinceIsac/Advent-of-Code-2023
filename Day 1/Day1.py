38e07e7c5dfa71f53dce05fc558b4593fb1a49c1
Prince <prince@synthesis.co.za>
2023-12-01 13:54:23 +0200

    Day 1 Completed

--- Day 1/Day1.py
+++ Day 1/Day1.py
@@ -1,5 +1,5 @@
-def read_input_file_generate_integer_list():
-    with open("C:/Users/PrinceLuhanga/Desktop/Personal/Advent of Code 2023/Advent-of-Code-2023/Day 1/Day1_Input.txt",
+def read_input_file_and_generate_integer_list():
+    with open('Day1_Input.txt',
               "r") as file:
         contents = file.read()
         lines = contents.splitlines()
@@ -14,3 +14,18 @@ def read_input_file_generate_integer_list():
     return array_of_lines_without_alphas
 
 
+def sum_list(list_of_lists):
+    total = 0
+    for current_list in list_of_lists:
+        string_concat = ""
+        if len(current_list) == 1:
+            string_concat += str(current_list[0]) + str(current_list[0])
+            total += int(string_concat)
+        else:
+            string_concat += str(current_list[0]) + str(current_list[-1])
+            total += int(string_concat)
+
+    return total
+
+
+print(sum_list(read_input_file_and_generate_integer_list()))
