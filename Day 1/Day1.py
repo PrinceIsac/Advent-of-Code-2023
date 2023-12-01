7aab7d44d1e09afe940365261b5a5f66d2f5a469
Prince <prince@synthesis.co.za>
2023-12-01 13:08:27 +0200

    Added logic to read txt and generate list

--- Day 1/Day1.py
+++ Day 1/Day1.py
@@ -1,13 +1,16 @@
-def read_input_file:
-  file = open("Day1_Input.text","r")
-  print(file)
+def read_input_file_generate_integer_list():
+    with open("C:/Users/PrinceLuhanga/Desktop/Personal/Advent of Code 2023/Advent-of-Code-2023/Day 1/Day1_Input.txt",
+              "r") as file:
+        contents = file.read()
+        lines = contents.splitlines()
+        array_of_lines_without_alphas = []
+        for line in lines:
+            temp_list = []
+            for char in line:
+                if char.isnumeric():
+                    temp_list.append(int(char))
+            array_of_lines_without_alphas.append(temp_list)
+
+    return array_of_lines_without_alphas
 
 
-def string_to_character_array:
-  return array;
-
-
-def remove_none_digit_character_:
-
-
-read_input_file()
